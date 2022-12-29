39bd0251ed610e19489c0b7657570db77183a15a
Ally Juma Said <mahadsaid95@icloud.com>
2022-12-29 22:17:42 +0000

    experimenting API use

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const wee = 9 ;
-if (wee > 8) {
-    console.log("False")
-}else{
-    console.log("true")
-}
+const express = require('express');
+const app = express();
+const router = require('./routes');
+
+app.use(router);
+
+
+
+app.listen(4000,()=> console.log('app running on http://localhost:4000'));
