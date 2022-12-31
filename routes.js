6b3286eb61712699a4742054dcd5256f215e7ceb
Ally Juma Said <mahadsaid95@icloud.com>
2022-12-31 21:46:12 +0000

    adding a POST request

--- routes.js
+++ routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get('/',(req,res)=>{
     res.send("connected successfully");
 })
+router.post('',(req,res)=>{
+    res.create({})
+})
 
 
 module.exports=router;
